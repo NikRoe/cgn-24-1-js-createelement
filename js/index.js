168b0816ff7e24efc461abe17b240621e57270f8
Niklas Röttger <niklas.roettger@outlook.de>
2024-01-24 10:28:12 +0100

    add session code

--- js/index.js
+++ js/index.js
@@ -3,3 +3,33 @@ console.clear();
 const form = document.querySelector('[data-js="card-form"]');
 const textInput = document.querySelector('[data-js="text-input"]');
 const cardContainer = document.querySelector('[data-js="card-container"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Approach with innerHTML
+
+  cardContainer.innerHTML = `
+  <li class="card">
+  <h2>Card</h2>
+  <p>${textInput.value}</p>
+  </li>
+  `;
+
+  // Approach with .createElement
+
+  // clearing the cardContainer
+  // cardContainer.innerHTML = "";
+
+  // creating a new list item
+  // const newCard = document.createElement("li");
+
+  // adding a class to newCard
+  //   newCard.classList.add("card");
+
+  // updating the textContent of newCard
+  //   newCard.textContent = textInput.value;
+
+  // adding newCard to the DOM as a child of cardContainer
+  // cardContainer.append(newCard);
+});
